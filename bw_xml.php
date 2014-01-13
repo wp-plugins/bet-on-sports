f06111fe3f63c3e7196f913e37263f721e5dacfc
SEOAlbion <SEOAlbion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-13 12:42:43 +0000

    ver 1
    git-svn-id: https://plugins.svn.wordpress.org/bet-on-sports/trunk@837646 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bw_xml.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+class Bw_xml {
+
+	public function get_live() {
+		if ($parse_type == 'simplexml_load_file')
+			$xml = simplexml_load_file('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/live.xml');
+
+		if ($parse_type == 'file_get_contents')
+			$xml = file_get_contents('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/live.xml');
+		return $xml;
+	}
+
+	public function _getlivestream() {
+		if ($parse_type == 'simplexml_load_file')
+			$xml = simplexml_load_file('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/livestream.xml');
+
+		if ($parse_type == 'file_get_contents')
+			$xml = file_get_contents('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/livestream.xml');
+		return $xml;
+	}
+
+	public function get_results() {
+
+		if ($parse_type == 'simplexml_load_file')
+			$xml = simplexml_load_file('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/results.xml');
+
+		if ($parse_type == 'file_get_contents')
+			$xml = file_get_contents('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/results.xml');
+		return $xml;
+	}
+
+	public function get_sports($parse_type = 'simplexml_load_file') {
+		if ($parse_type == 'simplexml_load_file')
+			$xml = simplexml_load_file('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/sports.xml');
+
+		if ($parse_type == 'file_get_contents')
+			$xml = file_get_contents('http://www.bukmekerskajakontora.ru/wp-content/themes/gameday/xml/sports.xml');
+		return $xml;
+	}
+
+}
+
+?>
