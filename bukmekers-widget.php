6d4228463dace096d22d2c563ced3409c0ffc068
SEOAlbion <SEOAlbion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-13 12:41:56 +0000

    ver 1
    git-svn-id: https://plugins.svn.wordpress.org/bet-on-sports/trunk@837645 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bukmekers-widget.php
@@ -0,0 +1,56 @@
+﻿<?php
+/*
+  Plugin Name: Ставки на спорт. Прогнозы.
+  Plugin URI: http://www.bukmekerskajakontora.ru/
+  Description: Таблицы, ставки на спорт, прогнозы. Два виджета, верхний и боковой, где выводятся коэфициенты ставок букмекерской конторы bet at home, посетители на вашем сайте смогут делать сразу ставки! Партнерскую ссылку можно менять, можно выводить разные виды спорта.
+  Version: 1.2
+  Author: SEOAlbion
+  Author URI: http://www.bukmekerskajakontora.ru/
+  Tags: widgets, Stakes
+ */
+
+register_activation_hook(__FILE__, 'bw_set_options');
+register_deactivation_hook(__FILE__, 'bw_unset_options');
+
+$url = explode("/", get_bloginfo('url'));
+$uri = ($url[3]) ? $url[3] . '/' : '/';
+
+require 'bw-config.php';
+require 'bw-stakes.php';
+require 'bw-ajax.php';
+require 'bw-shortcode.php';
+
+function bw_set_options() {
+	Stakes::bw_set_options();
+}
+
+function bw_unset_options() {
+	Stakes::bw_unset_options();
+}
+
+if (is_admin()) {
+	add_action('admin_menu', array('Stakes', 'bw_admin_menu'));
+}
+
+add_shortcode('bw_get_id_tournament', array('Bw_shortcode', 'bw_get_tournament'));
+add_action('wp_print_styles', array('Stakes', 'bw_user_css'));
+add_action('wp_enqueue_scripts', array('Stakes', 'bw_user_js'));
+add_action('wp_footer', array('Stakes', 'bw_func_carusel'), 900);
+
+if (@$_GET['page'] == 'bw_main_page') {
+	add_action('admin_init', array('Stakes', 'bw_admin_js'));
+	add_action('admin_init', array('Stakes', 'bw_admin_css'));
+}
+
+require 'bw-functions.php';
+require 'widget/widget_top.php';
+require 'widget/widget_sports.php';
+
+add_action('wp_ajax_my_action', array('Bw_ajax', 'my_action_callback'));
+add_action('wp_ajax_nopriv_my_action', array('Bw_ajax', 'my_action_callback'));
+add_action('wp_ajax_set_default', array('Bw_ajax', 'set_default_callback'));
+add_action('wp_ajax_nopriv_set_default', array('Bw_ajax', 'set_default_callback'));
+
+add_action('widgets_init', create_function('', 'register_widget( "Bw_top_widget" );'));
+add_action('widgets_init', create_function('', 'register_widget( "Bw_sports_widget" );'));
+?>
