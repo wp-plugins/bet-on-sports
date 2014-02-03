59efc1ce882ff91ef7fe2a09f8417fc826616cbb
SEOAlbion <SEOAlbion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 08:26:51 +0000

    Upgrade to Version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/bet-on-sports/tags/2.0@850378 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bukmekers-widget.php
@@ -0,0 +1,63 @@
+<?php
+/*
+  Plugin Name: Stakes on sport. Prognoses.
+  Plugin URI: http://www.bukmekerskajakontora.ru/
+  Description: Tables, stakes on sport, prognoses. There are two widgets - the upper and the side ones, where the coefficients for stakes of bookmaker's office betathome are shown, the visitors of your site can enter stakes at once! The partner reference can be changed, there is an opportunity to screen different types of sport.
+  Version: 1.2
+  Author: SEOAlbion
+  Author URI: http://www.bukmekerskajakontora.ru/
+  Tags: widgets, Stakes
+ */
+
+register_activation_hook(__FILE__, 'bw_set_options');
+register_deactivation_hook(__FILE__, 'bw_unset_options');
+
+$url = explode("/", get_bloginfo('url'));
+$uri = ($url[3]) ? $url[3] . '/' : '/';
+add_action('plugins_loaded', 'myplugin_init');
+function myplugin_init() {
+     load_plugin_textdomain( 'bet-on-sports', false, dirname( plugin_basename( __FILE__ )).'/language/' );
+}
+
+
+require 'bw-config.php';
+require 'bw-stakes.php';
+require 'bw-ajax.php';
+require 'bw-shortcode.php';
+require 'bw-functions.php';
+
+function bw_set_options() {
+	Stakes::bw_set_options();
+}
+
+function bw_unset_options() {
+	Stakes::bw_unset_options();
+}
+
+if (is_admin()) {
+	add_action('admin_menu', array('Stakes', 'bw_admin_menu'));
+}
+
+add_shortcode('bw_get_id_tournament', array('Bw_shortcode', 'bw_get_tournament'));
+add_action('wp_print_styles', array('Stakes', 'bw_user_css'));
+add_action('wp_enqueue_scripts', array('Stakes', 'bw_user_js'));
+
+
+if (@$_GET['page'] == 'bw_main_page') {
+	add_action('admin_init', array('Stakes', 'bw_admin_js'));
+	add_action('admin_init', array('Stakes', 'bw_admin_css'));
+}
+
+
+require 'widget/widget_top.php';
+require 'widget/widget_sports.php';
+
+add_action('wp_ajax_my_action', array('Bw_ajax', 'my_action_callback'));
+add_action('wp_ajax_nopriv_my_action', array('Bw_ajax', 'my_action_callback'));
+add_action('wp_ajax_set_default', array('Bw_ajax', 'set_default_callback'));
+add_action('wp_ajax_nopriv_set_default', array('Bw_ajax', 'set_default_callback'));
+add_action('wp_ajax_update_lang', array('Bw_ajax', 'update_lang_callback'));
+
+add_action('widgets_init', create_function('', 'register_widget( "Bw_top_widget" );'));
+add_action('widgets_init', create_function('', 'register_widget( "Bw_sports_widget" );'));
+?>
---
+++ widget/widget_top.php
@@ -0,0 +1,136 @@
+<?php
+/* Верхний виджет */
+
+class Bw_top_widget extends WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+			'Bw_top_widget', // идентификатор виджета
+			'Stakes Widget Top', // название виджета
+			array('description' => __('Stakes Widget Top - shows the list of common stakes in the form of slider with tab cards','bet-on-sports')) // Опции
+		);
+	}
+
+	public function widget($args, $instance) {
+		global $wpdb;
+		$table = get_option('BW_table_active');
+		if ($table == 'item') {
+			$sport_table = $wpdb->prefix . 'bw_sports';
+			$cat_table = $wpdb->prefix . 'bw_category';
+			$tourn_table = $wpdb->prefix . 'bw_tournament';
+			$item_table = $wpdb->prefix . 'bw_item';
+		}
+		if ($table == 'item_1') {
+			$sport_table = $wpdb->prefix . 'bw_sports_1';
+			$cat_table = $wpdb->prefix . 'bw_category_1';
+			$tourn_table = $wpdb->prefix . 'bw_tournament_1';
+			$item_table = $wpdb->prefix . 'bw_item_1';
+		}
+
+		$item = array();
+		if (get_option('BW_top_widget_cat') == 'all'):
+			$WHERE = 'WHERE ';
+			$WHERES = '';
+		else:
+			$WHERE = 'WHERE `ID_sport` IN(' . get_option('BW_top_widget_cat') . ') &&';
+			$WHERES = 'WHERE `ID_sport` IN(' . get_option('BW_top_widget_cat') . ')';
+		endif;
+
+		$res = $wpdb->get_results("SELECT * FROM $sport_table  $WHERES ORDER BY `ID_sport` ");
+		$res2 = $wpdb->get_results("SELECT * FROM $cat_table   $WHERES ORDER BY `name_cat` "); //JOIN $sport_table ON `$cat_table`.`ID_sport`=`$sport_table`.`ID_sport` ORDER BY `$cat_table`.`ID_sport`
+		$res3 = $wpdb->get_results("SELECT * FROM $tourn_table ORDER BY `name_tourn` ");
+		$param = Bw_shortcode::get_arr_name();
+		$tip = $param['Tip'];
+		
+		$results = $wpdb->get_results("SELECT * FROM `$item_table`  $WHERE  `betType`='$tip' ORDER BY `ID_sport` ");
+		foreach ($results as $data) {
+			//preg_match('#<OddsType betTypeId="(.+?)" oddType="(.+?)" betTypeGroup="(.+?)" defaultName="(.+?)" groupName="(.+?)" betName="(.+?)">(.+?)</OddsType>#is', $data->text, $_OddsType);
+			preg_match('#<OddsData>(.+?)</OddsData>#is', $data->text, $OddsData);
+			preg_match('#<HomeTeam>(.+?)</HomeTeam>#is', $OddsData[0], $HomeTeam);   #<HomeTeam>...</HomeTeam>
+			preg_match('#<AwayTeam>(.+?)</AwayTeam>#is', $OddsData[0], $AwayTeam);   #<AwayTeam>...</AwayTeam>
+			preg_match('#<HomeOdds oddId=".*" linkId=".*">(.+?)</HomeOdds>#is', $OddsData[0], $HomeOdds);  #<HomeOdds oddId="..." linkId="...">...</HomeOdds>
+			preg_match('#<AwayOdds oddId=".*" linkId=".*">(.+?)</AwayOdds>#is', $OddsData[0], $AwayOdds);  #<DrawOdds oddId="..." linkId="..."...</DrawOdds>
+			preg_match('/<Sport id="(.+?)">(.+?)<\/Sport>/iUs', $data->text, $_Sports);
+			$item[str2url($_Sports[2])]['compitention'][] = array(
+				'home' => @$HomeTeam[1],
+				'away' => @$AwayTeam[1],
+				'HomeOdds' => @$HomeOdds[1],
+				'AwayOdds' => @$AwayOdds[1],
+				'sport' => @$_Sports[2]
+			);
+		}
+		?>
+		<div class="bw"> 
+
+			<div class="bw-button-left"><a href="javascript:void(0);"> </a></div> 
+			<div class="bw-button-right"><a href="javascript:void(0);"> </a></div> 
+
+			<ul class="bw-tabs">
+				<?php
+				$s = 0;
+				foreach ($item as $key => $data) {
+					if ($s == 0)
+						$class = 'current';
+					else
+						$class = '';
+
+					$array_temp = array();
+					foreach ($data['compitention'] as $value) {
+
+						if ($value['sport'] != '')
+							if (!in_array($value['sport'], $array_temp)) {
+								$array_temp[] = $value['sport'];
+								echo "<li bw-items='" . str2url($value['sport']) . "' class='$class'>{$value['sport']}</li>";
+							}
+					}
+					$s++;
+				}
+				?>
+			</ul>
+
+			<div class="bw-wrapper"> 
+				<div class="bw-items"> 
+				</div>
+			</div>
+			<div class="none-show">
+				<?php
+				foreach ($item as $key => $data) {
+					echo '<div id="' . $key . '">';
+					foreach ($data['compitention'] as $value) {
+						?>
+						<div class="bw-block">
+							<a href="<?php echo get_option('bw_link_top'); ?>">
+								<span class="bw-score-status">16/12 12:00</span>
+								<div class="bw-score-teams">
+									<?php echo $value['home']; ?><br><?php echo $value['away']; ?>
+
+								</div><!--bw-score-teams-->
+								<div class="bw-score-right">
+									<?php echo $value['HomeOdds']; ?><br>
+									<?php echo $value['AwayOdds']; ?>
+								</div><!--bw-score-right-->
+							</a>
+						</div>
+						<?php
+					}
+					echo '</div>';
+				}
+				?>
+			</div>
+		</div>
+
+
+
+		<?php
+	}
+
+	public function update($new_instance, $old_instance) {
+		// ...
+		return $new_instance;
+	}
+
+	public function form($instance) {
+		// ...
+	}
+
+}
